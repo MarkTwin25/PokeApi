a34f522f9a506f5b287109be542774a8114b9cfd
MarkTwin25 <jesusmartin-25@ciencias.unam.mx>
2025-09-25 12:10:16 -0600

    First version

---
+++ app.js
@@ -0,0 +1,121 @@
+const typesSection = document.querySelector(".types");
+const pokemons = document.querySelector(".pokemons");
+
+
+async function getTypes(){
+    const response = await fetch("https://pokeapi.co/api/v2/generation/1/");
+    const data = await response.json();
+    const types = await data.types;
+    const listTypes = [];
+
+    types.forEach(element => {
+         listTypes.push(element.name)
+    });
+    
+    return listTypes;
+}
+
+async function createButtons(){
+    let types = await getTypes();
+    // Poner en title case
+    types = types.map(element => element[0].toUpperCase() + element.slice(1));
+
+    types.forEach(element => {
+        const div = createButton(element)
+        typesSection.appendChild(div);
+    })
+}
+
+function createButton(element){
+    const div = document.createElement("div");
+    div.classList.add("type");
+    div.classList.add(element.toLowerCase());
+    div.textContent = element;
+    return div;
+}
+
+async function getPokemonsLinks(){
+    const req = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    const data = await req.json();
+
+    var pokemonList = data.results;
+    pokemonList = pokemonList.map(pokemon => pokemon.url)
+    return pokemonList;
+}
+
+
+async function createAllCards(){
+    const links = await getPokemonsLinks();
+    const promises = links.map(link => fetch(link).then(data => data.json()))
+    const res = await Promise.all(promises);
+    
+    for(const pokemon of res){
+        const newCard = createCard(pokemon);
+        pokemons.appendChild(newCard)
+        
+    }
+}
+
+async function display(filter){
+    let selectPokemons = document.querySelectorAll(".pokemon");
+    if(selectPokemons.length ===0){
+        await createAllCards();
+        selectPokemons = document.querySelectorAll(".pokemon");
+    }
+
+    if(filter === "see all"){
+        Array.from(selectPokemons).forEach(el =>{
+            el.classList.remove("hidden");
+    });
+        return;
+    }
+    else{
+        Array.from(selectPokemons).forEach(el =>{
+            if(!el.classList.contains(`${filter}-search`)){
+                el.classList.add("hidden");
+            }else{
+                el.classList.remove("hidden");
+            }
+    });
+    }
+}
+
+function createCard(element){
+    const pokemon = document.createElement("div");
+    pokemon.classList.add("pokemon");
+    pokemon.classList.add(`${element.types[0].type.name}-search`)
+    
+    const container = document.createElement("div");
+    container.classList.add("container");
+    container.classList.add(`${element.types[0].type.name}`)
+
+    const h1 = document.createElement("h1");
+    h1.classList.add("name")
+    h1.textContent = element.name;
+    
+    const p = document.createElement("p");
+    p.classList.add("pokemon-type");
+    p.textContent = element.types[0].type.name;
+
+    const img = document.createElement("img");
+    img.src = element.sprites.other.dream_world.front_default;
+
+    container.appendChild(img)
+    container.appendChild(h1);
+    container.appendChild(p);
+
+    pokemon.appendChild(container);
+
+    return pokemon;
+
+}
+
+typesSection.addEventListener("click", async (e)=> {
+    if(e.target.classList.contains("type")){
+        const textLower = e.target.textContent.toLowerCase();
+        display(textLower);
+    }
+})
+
+
+createButtons();
